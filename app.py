8b2d11a5a2f0c359eed4f9a385ae5216b11918c4
Luis Tomas Bolivar <ltomasbo@redhat.com>
2024-03-13 14:44:21 +0100

    Test gradio example

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask
 import os
+import gradio as gr
 
-app = Flask(__name__)
+from openai import OpenAI
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+URL = "https://neural-magic-llm-poc-predictor-openvino.apps.cluster-h9jwq.dynamic.redhatworkshops.io"
 
-if __name__ == '__main__':
-    port = os.environ.get('FLASK_PORT') or 8080
-    port = int(port)
+def get_answer(question, url):
+    client = OpenAI(base_url=url, api_key="EMPTY")
+    model = client.models.list().data[0][1]
+    print(f"Accessing model API '{model}'")
 
-    app.run(port=port,host='0.0.0.0')
+    completion = client.completions.create(model=model, prompt=question, max_tokens=100, temperature=0.2)
+    return completion.choices[0].text
+
+port = os.environ.get('FLASK_PORT') or 8080
+
+iface = gr.Interface(fn=get_answer,
+                     inputs=["text", gr.Dropdown(choices=[URL])],
+                     outputs="text")
+
+iface.launch(server_port=port)
